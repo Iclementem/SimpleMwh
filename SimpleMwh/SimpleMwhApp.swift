34ef00367442447e5308530f656c8be713be61d2
Iclementem <iclementem@outlook.es>
2023-02-02 12:29:46 +0100

    Initial Commit

---
+++ SimpleMwh/SimpleMwhApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMwhApp.swift
+//  SimpleMwh
+//
+//  Created by Iván Clemente Moreno on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMwhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
